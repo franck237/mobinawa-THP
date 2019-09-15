8f176a6719e5c233d4a3d4f0bfa2f1b56927dcf4
franck237 <franck.kamanou@gmail.com>
2019-09-15 17:23:46 +0200

    Adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,16 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
-  get 'products/new'
-  get 'products/create'
-  get 'products/edit'
-  get 'products/update'
-  get 'products/destroy'
-  get 'companies/index'
-  get 'companies/show'
-  get 'companies/new'
-  get 'companies/create'
-  get 'companies/edit'
-  get 'companies/update'
-  get 'companies/destroy'
-  get 'admins/index'
-  get 'admins/show'
-  get 'admins/new'
-  get 'admins/create'
-  get 'admins/edit'
-  get 'admins/update'
-  get 'admins/destroy'
-  get 'sub_sectors/index'
-  get 'sub_sectors/show'
-  get 'sub_sectors/new'
-  get 'sub_sectors/create'
-  get 'sub_sectors/edit'
-  get 'sub_sectors/update'
-  get 'sub_sectors/destroy'
-  get 'sectors/index'
-  get 'sectors/show'
-  get 'sectors/new'
-  get 'sectors/create'
-  get 'sectors/edit'
-  get 'sectors/update'
-  get 'sectors/destroy'
-  get 'countries/index'
-  get 'countries/show'
-  get 'countries/new'
-  get 'countries/create'
-  get 'countries/edit'
-  get 'countries/update'
-  get 'countries/destroy'
   devise_for :admins
+  root to: 'sectors#index'
+
+  resources :admins do
+    resources :compagnies
+  end
+
+  resources :sectors do
+    resources :sub_sectors do
+      resources :compagnies do
+      resources :products
+      end
+    end
+  end
 end
