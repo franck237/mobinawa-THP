fd7eca8f7d5443c90dbf7c2dc86c557d9916f8bf
franck237 <franck.kamanou@gmail.com>
2019-09-15 17:12:41 +0200

    Creating a controller for companies

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'companies/index'
+  get 'companies/show'
+  get 'companies/new'
+  get 'companies/create'
+  get 'companies/edit'
+  get 'companies/update'
+  get 'companies/destroy'
   get 'admins/index'
   get 'admins/show'
   get 'admins/new'
