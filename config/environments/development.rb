ecbf49946627ba22f29b793e9d9693dfac1db99c
ekwayv8 <yvon.ekwalla@gmail.com>
2019-09-19 08:51:32 +0200

    Removing letter opener from the development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.delivery_method = :letter_opener
+  #config.action_mailer.delivery_method = :letter_opener
 
   # If false, no email will be sent by the app
   config.action_mailer.perform_deliveries = true
