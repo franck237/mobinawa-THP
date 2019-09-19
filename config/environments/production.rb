2da2d91b72422c40f96ac93a2a05bb36acd46ae6
ekwayv8 <yvon.ekwalla@gmail.com>
2019-09-19 09:50:14 +0200

    Modification of the environment file and production file in order to run the adminmailer in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ config.assets.digest = true
   # config.active_job.queue_name_prefix = "mobinawa_testing_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => 'mobinawa-development.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'mobinawa.herokuapp.com' }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
