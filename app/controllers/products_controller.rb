b26940181d5e3856ea94c04a48dcf2e37de46dc8
franck237 <franck.kamanou@gmail.com>
2019-09-19 14:59:05 +0200

    Configuration of the product controller & product#index view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,22 +1,33 @@
 class ProductsController < ApplicationController
+before_action :authenticate_admin!, except: [:index, :show]
   def index
+    @products = Product.all  
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def new
+    @product = Product.new 
   end
 
   def create
-  end
-
-  def edit
+    @product = Product.new(title: params[:title], description: params[:description], price: params[:price], image_product: params[:image_product], quantity: params[:quantity], status: params[:status])
+      if @product.save
+        redirect_to admin_product_path(@product.id)
+      else
+        render 'new'
+      end
   end
 
   def update
+  
   end
 
   def destroy
+    @product = Product.destroy
+
   end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -7,8 +7,8 @@ class Product < ApplicationRecord
 	#Description validation: it is mendatory to have an description for a Product
 	validates_length_of :description, minimum: 5
 
-    #Price validation: it is mendatory to have a price for a product and also that price need to be decimal
-    validates :price, numericality: { greater_than_or_equal_to: 0 }
+  #Price validation: it is mendatory to have a price for a product and also that price need to be decimal
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
 
 	#Image_product validation: it is mendatory to have an image for a Product
 	validates :image_product,
@@ -23,7 +23,8 @@ class Product < ApplicationRecord
 	validates :status, inclusion: { in: [ true, false ] }
 
 
-	#Associations: One company belong to one Admin, une sub_sector and one country	
+	#Associations: One product belong to one Company	
 	belongs_to :company
+	delegate :admin, to: :company
 
 end
