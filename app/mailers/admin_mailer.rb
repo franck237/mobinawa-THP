06f4f78c1a923d6aa37075b3c6f81e2a0679ca1d
ekwayv8 <yvon.ekwalla@gmail.com>
2019-09-19 08:40:14 +0200

    Adding a method to the adminMailer

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,2 +1,10 @@
 class AdminMailer < ApplicationMailer
+
+default from: 'no-reply@mobinawa.herokuapp.com'
+ 
+  def welcome_email(admin)
+    @admin = admin 
+    @url  = 'https://mobinawa-development.herokuapp.com' 
+    mail(to: @admin.email, subject: 'Bienvenue au sein du réseau Mobinawa') 
+  end
 end
